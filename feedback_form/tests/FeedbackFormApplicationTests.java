ee4c083ddeb1bb29e9f82cc7332027706357310f
02Tirtha <160453974+02Tirtha@users.noreply.github.com>
2025-06-03 21:30:49 +0530

    Add files via upload

---
+++ feedback_form/tests/FeedbackFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.feedback_form;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
